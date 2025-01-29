8279010adc90af223194e43741e40107ea56d478
carloseduardoxp <carloseduardoxpto@gmail.com>
2025-01-29 18:59:47 -0300

    adicionando cors

--- src/main/java/br/edu/iftm/tspi/pmvc/login/LoginApplication.java
+++ src/main/java/br/edu/iftm/tspi/pmvc/login/LoginApplication.java
@@ -2,6 +2,10 @@ package br.edu.iftm.tspi.pmvc.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LoginApplication {
@@ -10,4 +14,24 @@ public class LoginApplication {
 		SpringApplication.run(LoginApplication.class, args);
 	}
 
+	@Configuration
+	public static class CorsConfiguracao {
+
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+						.allowedMethods("HEAD","GET","PUT","POST","DELETE","PATCH");
+			}
+			};
+
+			
+		}
 }
+
+}
+
+
